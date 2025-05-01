6752c6ebce0b21fadf828ea5d8a1f6d0205618b2
Anujika Ambagahawatta <119099823+AnujikaSasindi@users.noreply.github.com>
2025-05-01 17:39:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /**
  * @type {import('next').NextConfig}
  */
-const isProd = process.env.NODE_ENV = 'production';
 
  const nextConfig = {
-    basePath: isProd ? '/SenseGridWeb' : '',
     output: 'export',
     distDir: 'dist',
     images: {
